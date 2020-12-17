214f040441b00beaf2f941f574c291c78740008b
LenaChen-zhu <qiangxiao_1984@qq.com>
2020-12-17 23:11:24 +0800

    add getAllText and getIndexText method

--- src/main/java/com/po/util/ActionsUtil.java
+++ src/main/java/com/po/util/ActionsUtil.java
@@ -3,6 +3,10 @@ package com.po.util;
 import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActionsUtil {
     //打开url网址
@@ -29,6 +33,25 @@ public class ActionsUtil {
     public static String getText(WebDriver driver,long timeout,By by){
         return FindElementsUtil.findElement(driver,timeout,by).getText();
     }
+
+    //获取一系列空间的文本
+    public static ArrayList getAllText(WebDriver driver, long timeout, By by){
+        List<WebElement> elementList = FindElementsUtil.findElements(driver,timeout,by);
+        ArrayList arrayList = new ArrayList();
+        for (int i = 0; i < elementList.size(); i++) {
+            arrayList.add(elementList.get(i).getText());
+        }
+        return arrayList;
+    }
+
+    //获取指定位置的文本
+    public static String getIndexText(WebDriver driver,long timeout,By by,int index){
+        List<WebElement> elementList = FindElementsUtil.findElements(driver,timeout,by);
+        return elementList.get(index).getText();
+    }
+
+
+
 
 
 
