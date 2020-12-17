46f913f6d3812416509a24705975e007ffcb22d7
LenaChen-zhu <qiangxiao_1984@qq.com>
2020-12-17 20:57:59 +0800

    edit DriverUtilTest and FindElementsUtilTest add try{}catch{}finally{} methods.

--- src/test/java/com/po/util/DriverUtilTest.java
+++ src/test/java/com/po/util/DriverUtilTest.java
@@ -5,9 +5,14 @@ import org.testng.annotations.Test;
 public class DriverUtilTest {
 
     @Test
-    public void openTest() throws Exception {
-        DriverUtil.open("Chrome");
-        Thread.sleep(2000);
-        DriverUtil.closeAll();
+    public void openTest(){
+        try {
+            DriverUtil.open("Chrome");
+            Thread.sleep(2000);
+        }catch (Exception e){
+            System.out.println("打开浏览器失败"+e.getMessage());
+        }finally {
+            DriverUtil.closeAll();
+        }
     }
 }
--- src/test/java/com/po/util/FindElementsUtilTest.java
+++ src/test/java/com/po/util/FindElementsUtilTest.java
@@ -1,19 +1,32 @@
 package com.po.util;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
 
 public class FindElementsUtilTest {
     private WebDriver driver;
     private String url = "https://www.baidu.com/";
 
     @Test
-    public void findElementTest() throws InterruptedException {
-        driver = DriverUtil.open("Chrome");
-        driver.get(url);
-        FindElementsUtil.findElement(driver,3, By.id("kw")).sendKeys("Selenium");
-        Thread.sleep(3000);
-        DriverUtil.closeAll();
+    public void findElementTest() {
+        try{
+            driver = DriverUtil.open("Chrome");
+            driver.get(url);
+            FindElementsUtil.findElement(driver,3, By.id("kw")).sendKeys("Selenium");
+            FindElementsUtil.findElement(driver,2,By.id("su")).click();
+            String text = FindElementsUtil.findElement(driver,2,By.linkText("MeterSphere - 开源自动化测试平台")).getText();
+            Assert.assertEquals("MeterSphere - 开源自动化测试平台",text);
+        }catch (Exception e){
+            System.out.println("没有找到对应的元素"+e.getMessage());
+            e.printStackTrace();
+        }finally {
+            DriverUtil.closeAll();
+        }
+
+
+
     }
 }
